de1dbe9ca584a88c5a2770dfb15482c140f2dffd
Igor Nehoroshev <mail@neigor.me>
2020-10-06 19:10:13 +0300

    Force rollback if TESTING is set to True

--- melodiam/resources.py
+++ melodiam/resources.py
@@ -3,5 +3,5 @@ from sqlalchemy import MetaData  # type: ignore[import]
 
 from melodiam import conf
 
-database = Database(conf.DATABASE_URL)
+database = Database(conf.DATABASE_URL, force_rollback=conf.TESTING)
 metadata = MetaData()
