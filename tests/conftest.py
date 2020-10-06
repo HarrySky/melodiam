1cdbc271a5cbc8a9e29df7c6fdc3e2032adaeeb2
Igor Nehoroshev <mail@neigor.me>
2020-10-06 18:30:39 +0300

    Add test for full melodiam-auth authentication sequence

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from asyncio import get_event_loop_policy
 from logging import getLogger
 
 import pytest
+from httpx import AsyncClient
 
 
 def no_problems_logged(capfd) -> bool:
@@ -19,6 +20,21 @@ def clear_logger():
     handlers = getattr(logger, "handlers", [])
     for handler in handlers:
         logger.removeHandler(handler)
+
+
+@pytest.fixture(scope="session")
+async def auth_client():
+    from melodiam.auth import api, endpoints
+
+    await endpoints.startup()
+    # Patch sender
+    endpoints._credentials.sender.client = endpoints._api.sender.client = AsyncClient(
+        app=api, base_url="https://accounts.spotify.com"
+    )
+    async with AsyncClient(app=api, base_url="http://localhost:7455") as client:
+        yield client
+
+    await endpoints.shutdown()
 
 
 @pytest.yield_fixture(scope="session")
